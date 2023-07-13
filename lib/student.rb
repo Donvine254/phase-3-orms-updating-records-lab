aca10432f547cbec8f6a500487fd02eb5174e5d8
donvine mugendi <donvinemugendi@gmail.com>
2023-07-13 15:12:34 +0300

    completed lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,37 +1,76 @@
-require_relative "../config/environment.rb"
+# require_relative "../config/environment.rb"
+require 'sqlite3'
+
+require 'sqlite3'
 
 class Student
-  attr_accessor :id , :name, :grade
-  def initialize (id=nil, name, grade)
+  DB = {
+    conn: SQLite3::Database.new('db/students.db')
+  }
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(id=nil, name, grade)
     @id = id
     @name = name
     @grade = grade
   end
+
   def self.create_table
-    sql =  <<-SQL 
+    sql = <<-SQL 
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
         name TEXT,
-        grade INTEGER
-        )
-        SQL
+        grade TEXT
+      )
+    SQL
     DB[:conn].execute(sql)
   end
+
   def save
-    sql = <<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?, ?) 
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
-  def self.create(name:, grade:)
+
+  def self.create (name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students;"
+    DB[:conn].execute(sql)
+    puts "Table dropped successfully."
+  end
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+
+    Student.new(id, name, grade)
+  end
 end
 
-Student.create_table
-student = Student.create(name: "John Doe", grade: 10)
+
+# Student.create_table
+# student = Student.create(name: "John Doe", grade: 10)
+# Student.drop_table
